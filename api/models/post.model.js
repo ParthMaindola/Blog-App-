29efcee5e968c9c0991edd75158ac380f54acc23
Parth <parthmaindola@gmail.com>
2024-07-21 17:54:03 +0530

    added create a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        image: {
+            type: String,
+            default: 'https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png',
+        },
+        category: {
+            type: String,
+            default: 'uncategorized',
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        }
+    }, {timestamps: true}
+);
+
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
