90cabaadd32b1321e7b8b837c30dd07d70b5568d
Parth <parthmaindola@gmail.com>
2024-07-23 18:17:32 +0530

    added like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
 import { Await, Link } from 'react-router-dom';
 import Comment from './Comment';
+import { useNavigate } from 'react-router-dom';
 
 export default function CommentSection({postId}) {
 
@@ -10,7 +11,9 @@ export default function CommentSection({postId}) {
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate(); 
     console.log(comments)
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -52,7 +55,36 @@ export default function CommentSection({postId}) {
               }
             }
             getComments();
-        }, [postId])
+        }, [postId]);
+
+
+        const handleLike = async (commentId) => {
+          try {
+            if (!currentUser) {
+              navigate('/sign-in');
+              return;
+            }
+            const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+              method: 'PUT',
+            });
+            if (res.ok) {
+              const data = await res.json();
+              setComments(
+                comments.map((comment) =>
+                  comment._id === commentId
+                    ? {
+                        ...comment,
+                        likes: data.likes,
+                        numberOfLikes: data.likes.length,
+                      }
+                    : comment
+                )
+              );
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+        }
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -115,6 +147,7 @@ export default function CommentSection({postId}) {
                   <Comment
                     key={comment._id}
                     comment={comment}
+                    onLike={handleLike}
                   />
                 ))
               }
